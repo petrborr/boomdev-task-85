1d8b5c3b607411dc90ffc5c42811464f70d820a7
petrborr <petrborr@gmail.com>
2022-12-19 14:10:11 +0200

    add: fetch pokemon names & display them as list items

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,14 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  // let names = []
   const ul = document.querySelector("ul");
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then(res => res.json())
+    .then(data => data['results']
+    .forEach(planet => {
+    const nameElement = document.createElement("li")
+    nameElement.innerText = planet["name"]
+    ul.appendChild(nameElement)
+    }))
 });
